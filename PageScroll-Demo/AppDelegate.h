fff5e65ca3f408a1a4788f57a1dbcab205585eb9
高考这件小事 <pupboss.vip@gmail.com>
2015-03-27 20:37:52 +0800

    Initial Commit

---
+++ PageScroll-Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PageScroll-Demo
+//
+//  Created by JieLee on 15/3/27.
+//  Copyright (c) 2015年 PUPBOSS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
